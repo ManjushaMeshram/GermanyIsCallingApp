4389561654e6c1c4265e66f5f9a39a28121097d8
Manjusha <manjushameshram95@gmailcom>
2024-09-10 17:32:27 +0530

    Source file added

---
+++ src/main/java/testrunner/RunPurnaTest.java
@@ -0,0 +1,22 @@
+package testrunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/main/java/feature"},
+        tags= "@GermanyLoginTest",
+        glue = {"stepdefinition"},
+        
+//      dryRun = true,
+        monochrome = true,
+        publish = true      
+)
+public class RunPurnaTest {
+}
+ 
+
